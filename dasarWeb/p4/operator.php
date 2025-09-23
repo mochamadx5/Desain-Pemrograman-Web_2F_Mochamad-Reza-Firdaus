5f7cae3d5ab28e0ae7261256767879daef6971e0
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-09-23 23:54:15 +0700

    JOBSHEET 4_PRAKTIKUM 3_NO3.2

--- dasarWeb/p4/operator.php
+++ dasarWeb/p4/operator.php
@@ -15,4 +15,35 @@ echo "Hasil Kali: $hasilKali <br>";
 echo "Hasil Bagi: $hasilBagi <br>";
 echo "Sisa Bagi (modulus): $sisaBagi <br>";
 echo "Pangkat: $pangkat <br>";
+
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+
+echo "Apakah a sama dengan b? ";
+var_dump($hasilSama);
+echo "<br>";
+
+echo "Apakah a tidak sama dengan b? ";
+var_dump($hasilTidakSama);
+echo "<br>";
+
+echo "Apakah a lebih kecil dari b? ";
+var_dump($hasilLebihKecil);
+echo "<br>";
+
+echo "Apakah a lebih besar dari b? ";
+var_dump($hasilLebihBesar);
+echo "<br>";
+
+echo "Apakah a lebih kecil atau sama dengan b? ";
+var_dump($hasilLebihKecilSama);
+echo "<br>";
+
+echo "Apakah a lebih besar atau sama dengan b? ";
+var_dump($hasilLebihBesarSama);
+echo "<br>";
 ?>
