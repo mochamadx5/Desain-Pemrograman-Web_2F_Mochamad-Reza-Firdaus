02297e8da4e0942cfd026e201b7b8c267cf64646
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-09-23 10:05:53 +0700

    jobsheet4_praktikum1_no1

---
+++ dasarWeb/p4/variabel_konstanta.php
@@ -0,0 +1,15 @@
+<?php
+$angka1 = 10;
+$angka2 = 5;
+$hasil = $angka1 + $angka2;
+echo "hasil penjumlahan $angka1 dan $angka2 adalah $hasil.";
+
+$benar = true;
+$salah = false;
+
+//mendefinisikan konstanta untuk nilai tetap
+define("nama_situs", "Websiteku.com");
+define("tahun_pendirian", 2023);
+echo "selamat datang di ". nama_situs . ", situs yang didirikan
+pada tahun " . tahun_pendirian . ".";
+?>
