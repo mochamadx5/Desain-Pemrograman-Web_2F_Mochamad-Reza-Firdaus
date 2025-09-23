6a04e564a522ba7a64b1b2c472f139072e695997
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-09-23 23:44:53 +0700

    jobsheet4_praktikum3_no3.1

---
+++ dasarWeb/p4/operator.php
@@ -0,0 +1,18 @@
+<?php
+$a = 10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisaBagi = $a % $b;
+$pangkat = $a ** $b;
+
+echo "Hasil Tambah: $hasilTambah <br>";
+echo "Hasil Kurang: $hasilKurang <br>";
+echo "Hasil Kali: $hasilKali <br>";
+echo "Hasil Bagi: $hasilBagi <br>";
+echo "Sisa Bagi (modulus): $sisaBagi <br>";
+echo "Pangkat: $pangkat <br>";
+?>
