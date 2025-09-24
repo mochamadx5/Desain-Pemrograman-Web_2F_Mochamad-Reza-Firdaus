590aeeb53f27c8a9f40b78b241a427ccf8ce1dd3
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-09-24 11:27:38 +0700

    jobsheet4_praktikum4_no4.8

--- dasarWeb/p4/skor_game.php
+++ dasarWeb/p4/skor_game.php
@@ -5,7 +5,7 @@ $totalSkor2 = 525;
 $hadiahTambahan = ($totalSkor > 500) ? "YA " : "TIDAK";
 echo "total skor pemain 1 adalah : $totalSkor <br>";
 
-echo "apakah pemain 1 mendapatkan hadiah tambahan? $hadiahTambahan <br>";
+echo "Apakah pemain 1 mendapatkan hadiah tambahan? $hadiahTambahan <br>";
 
 
 $hadiahTambahan = ($totalSkor2 > 500) ? "YA " : "TIDAK";
