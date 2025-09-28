92960a21d64874976a12ae1d77c46cc85dc86b68
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-09-28 22:23:49 +0700

    no1

---
+++ dasarWeb/p5-php1/fungsi.php
@@ -0,0 +1,11 @@
+<?php
+
+function perkenalan() {
+    echo "assalamualaikum,";
+    echo "Perkenalkan, nama saya Reza <br/>";
+    echo "Senang berkenalan dengan anda <br/>";
+}
+// memanggil fungsi yang sudah dibuat
+perkenalan();
+perkenalan();
+?>
