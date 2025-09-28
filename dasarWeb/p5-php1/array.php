2845fb6ab228bed662357a06810fabd76df50fc9
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-09-28 23:04:20 +0700

    no8

--- dasarWeb/p5-php1/array.php
+++ dasarWeb/p5-php1/array.php
@@ -1,43 +1,37 @@
 <?php
 $menu = [
-    [
-        "nama" => "Beranda"
-    ],
-    [
+    [ "nama" => "Beranda" ],
+    [ 
         "nama" => "Berita",
         "subMenu" => [
             [
                 "nama" => "Wisata",
                 "subMenu" => [
-                    [
-                        "nama" => "Pantai"
-                    ],
-                    [
-                        "nama" => "Gunung"
-                    ]
+                    [ "nama" => "Pantai" ],
+                    [ "nama" => "Gunung" ]
                 ]
             ],
-            [
-                "nama" => "Kuliner"
-            ],
-            [
-                "nama" => "Hiburan"
-            ]
+            [ "nama" => "Kuliner" ],
+            [ "nama" => "Hiburan" ]
         ]
     ],
-    [
-        "nama" => "Tentang"
-    ],
-    [
-        "nama" => "Kontak"
-    ]
+    [ "nama" => "Tentang" ],
+    [ "nama" => "Kontak" ]
 ];
-function tampilkanMenuBertingkat (array $menu) {
+
+function tampilkanMenuBertingkat(array $menu) {
     echo "<ul>";
-    foreach ($menu as $key => $item) {
-        echo "<li> {$item['nama']} </li>";
+    foreach ($menu as $item) {
+        echo "<li>{$item['nama']}";
+        // cek apakah ada subMenu
+        if (isset($item['subMenu'])) {
+            tampilkanMenuBertingkat($item['subMenu']); // panggil rekursif
+        }
+        echo "</li>";
     }
     echo "</ul>";
 }
+
+// memanggil fungsi utama
 tampilkanMenuBertingkat($menu);
 ?>
