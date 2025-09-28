21c7921d28253a0b530ac9b7a139f76d45ed2978
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-09-28 22:29:22 +0700

    no2

--- dasarWeb/p5-php1/fungsi.php
+++ dasarWeb/p5-php1/fungsi.php
@@ -1,11 +1,18 @@
 <?php
 
-function perkenalan() {
-    echo "assalamualaikum,";
-    echo "Perkenalkan, nama saya Reza <br/>";
+function perkenalan($nama, $salam) {
+    echo $salam. "," ;
+    echo "Perkenalkan, nama saya". $nama.  "<br/>";
     echo "Senang berkenalan dengan anda <br/>";
 }
 // memanggil fungsi yang sudah dibuat
-perkenalan();
-perkenalan();
+perkenalan(" hamdana", "hallo");
+
+echo "<hr>";
+
+$saya = " elok";
+$ucapanSalam = "Selamat pagi";
+
+// memanggil lagi
+perkenalan($saya, $ucapanSalam);
 ?>
