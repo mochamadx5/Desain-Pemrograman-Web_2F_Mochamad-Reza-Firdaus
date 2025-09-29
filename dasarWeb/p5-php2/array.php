46a9f7656f89bf10732c919c38cdc7589e1144be
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-09-29 22:56:11 +0700

    no2

--- dasarWeb/p5-php2/array.php
+++ dasarWeb/p5-php2/array.php
@@ -5,15 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+    body {
+        color: blue;
+        font-family : "consolas";
+    }
+</style>
 <body>
     <?php
     $Dosen = [
         'nama' => 'elok nur hamdana',
         'domisili' => 'Malang',
         'jenis kelamin' => 'Perempuan'];
-    foreach ($Dosen as $key => $value) {
-          echo $key . " : " . $value . "<br>";
-    }
+    echo "Nama :  {$Dosen ['nama']} <br>";
+    echo "Domisili : {$Dosen ['domisili']} <br>";
+    echo "Jenis kelamin : {$Dosen ['jenis kelamin']} <br>";
     ?>
 </body>
 </html>
