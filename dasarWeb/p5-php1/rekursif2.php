b599c78bfb6ddc220b61c230452686261de8513b
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-10-06 20:24:39 +0700

    no10

--- dasarWeb/p5-php1/rekursif2.php
+++ dasarWeb/p5-php1/rekursif2.php
@@ -2,7 +2,7 @@
 function tampilkanAngka(int $jumlah, int $indeks = 1) {
     echo "Perulangan ke-{$indeks} <br>";
 
-    //panggil diri sendiri selama $indeks <= jumlah
+    // Panggil diri sendiri selama $indeks <= jumlah
     if ($indeks < $jumlah) {
         tampilkanAngka($jumlah, $indeks + 1);
     }
