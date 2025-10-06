32d4d73fa22cd18fd1913f1361782fd75eafcf7c
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-10-06 20:41:59 +0700

    no18

---
+++ dasarWeb/p5-php1/entitas.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Entitas html</title>
+</head>
+<body>
+    <p>It&#39; time to read a HTML5 book. </p>
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;Keuntungan dari menggunakan nama entities:
+        Sebuah nama entities akan mudah diingat. Kerugian menggunakan nama entities : Browser
+        mungkin tidak mungkin semua nama entitas, tetapi dukungan untuk nomor lebih baik</p>
+</body>
+</html>
