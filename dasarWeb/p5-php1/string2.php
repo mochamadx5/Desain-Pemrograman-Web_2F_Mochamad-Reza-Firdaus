50e5c2e8e910c11cac5b043c091aa4950c0c4b9c
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-10-06 20:19:51 +0700

    no14

--- dasarWeb/p5-php1/string2.php
+++ dasarWeb/p5-php1/string2.php
@@ -7,6 +7,6 @@ echo "Halo\r Dunia <br>"; //soal 10.d
 echo "<pre>Halo\tDunia!</pre>"; //soal 10.e
 echo "<pre>Halo\tDunia!</pre>"; //soal 10.f
 
-echo "Katakanlah \"Tidak pada narkoba!\" <br>"; //soal 10.g
+echo "Katakanlah \"Tidak Pada narkoba!\" <br>"; //soal 10.g
 echo "Katakanlah \"Tidak pada narkoba!\" <br>"; //soal 10.h
 ?>
