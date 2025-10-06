e2f547bc3e4e43e3e44e01e4e6184e4e9426007d
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-10-06 20:20:47 +0700

    no15

--- dasarWeb/p5-php1/string3.php
+++ dasarWeb/p5-php1/string3.php
@@ -1,5 +1,5 @@
 <?php 
-$pesan = "Saya Arek Malang";
+$pesan = "Saya Arek  Malang";
 $pesanPerKata = explode(" ", $pesan);
 $pesanPerKata = array_map(fn($pesan) => strrev($pesan), $pesanPerKata);
 $pesan = implode( "", $pesanPerKata);
