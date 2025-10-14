ce8b19984419aea9b4da08e9a1ae335548104dbd
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-10-14 22:19:40 +0700

    UTS

---
+++ dasarWeb/UTS/pemulihan.js
@@ -0,0 +1,4 @@
+document.getElementById('btn-proses').addEventListener('click', function() {
+  window.location.href = 'https://www.polinema.ac.id';
+    }
+);
---
+++ dasarWeb/UTS/script.js
@@ -0,0 +1,20 @@
+// tampilkan password
+const togglePassword = document.querySelector('#showPassword');
+const passwordInput = document.querySelector('#password');
+
+togglePassword.addEventListener('change', function() {
+  passwordInput.type = this.checked ? 'text' : 'password';
+});
+
+// Login ke Beranda
+document.getElementById('loginBtn').addEventListener('click', function() {
+  const username = document.getElementById('username').value.trim();
+  const password = document.getElementById('password').value.trim();
+
+  if (username === '244107020104' && password === 'TI-2F') {
+    window.location.href = 'beranda.html';
+  } else {
+    alert('Username dan password tidak ditemukan');
+    return; 
+  }
+});
