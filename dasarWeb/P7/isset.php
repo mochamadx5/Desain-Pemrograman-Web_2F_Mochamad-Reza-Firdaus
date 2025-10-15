8e97fd27fc012dfd721c670aa2e81441b064dd12
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-10-15 09:20:42 +0700

    jobsheet7_no1.2

--- dasarWeb/P7/isset.php
+++ dasarWeb/P7/isset.php
@@ -3,6 +3,12 @@ $umur;
 if (isset($umur) && $umur >= 18) {
     echo "anda Sudah dewasa.";
 } else {
-    echo "Anada belum dewasa atau variabel 'umur tidak ditemukan";
+    echo "Anada belum dewasa atau variabel 'umur tidak ditemukan <br>";
+}
+$data = array("nama" => "Jane", "usia" => 25 );
+if (isset($data["nama"])) {
+    echo "Nama : " . $data["nama"];
+} else {
+    echo "variabel 'nama tidak ditemukan dalam array.";
 }
 ?>
