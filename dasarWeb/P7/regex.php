f59abb86ece4abe8890faf693153f7eab080f175
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-10-20 22:48:33 +0700

    soal no 5.2

--- dasarWeb/P7/regex.php
+++ dasarWeb/P7/regex.php
@@ -6,3 +6,11 @@ if (preg_match($pattern, $text)) {
 } else {
     echo "tidak ada huruf kecil";
 }
+$pattern = '/[0-9]+/'; // Cocokkan satu atau lebih digit.
+$text = 'There are 123 apples.';
+
+if (preg_match($pattern, $text, $matches)) {
+    echo "<br> <br> Cocokkan: " . $matches[0];
+} else {
+    echo "Tidak ada yang cocok!";
+}
