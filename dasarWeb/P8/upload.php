321018d3b78339bac49da0895a6ef1605c87d8c4
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-10-28 21:59:28 +0700

    jobsheet8_pertanyaanNO5

--- dasarWeb/P8/upload.php
+++ dasarWeb/P8/upload.php
@@ -9,7 +9,8 @@ if(isset($_POST["submit"])){
 
     if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize){
         if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)){
-            echo "File berhasil diunggah.";
+            echo "File berhasil diunggah. <br>";
+             echo "<img src='$targetfile' width='200' style='height:auto; margin-top:10px; border:1px solid #ccc;'>";
         } else {
             echo "Gagal mengunggah file.";
         }
