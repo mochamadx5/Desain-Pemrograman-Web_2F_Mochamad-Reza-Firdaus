10530350e65da24febacd503a9f7629410580ece
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2025-10-28 22:27:22 +0700

    jobsheet8_pertanyaan6-7

--- dasarWeb/P8/upload.php
+++ dasarWeb/P8/upload.php
@@ -4,13 +4,12 @@ if(isset($_POST["submit"])){
     $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
     $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
 
-    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
-    $maxsize = 5*1024*1024;
+    $allowedExtensions = array("txt", "pdf", "doc", "docx");
+    $maxsize = 3*1024*1024;
 
     if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize){
         if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)){
-            echo "File berhasil diunggah. <br>";
-             echo "<img src='$targetfile' width='200' style='height:auto; margin-top:10px; border:1px solid #ccc;'>";
+            echo "File berhasil diunggah";
         } else {
             echo "Gagal mengunggah file.";
         }
